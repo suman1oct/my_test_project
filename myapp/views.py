55d9ad6dfa46c25e90ae61951c3e8388eb07899c
suman <sumanymca@gmail.com>
2017-06-22 12:54:03 +0530

    Homepage view added

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class HomepageView(generic.TemplateView):
+	
+	"""
+	Homepage View
+	"""
+
+	# template_name = 'forum/index.html'
+
+	def get(self, *args, **kwargs):
+		"""
+		check if user is already loggedin then redirect to dashboard
+		"""
+
+		if self.request.user.is_authenticated():
+			return redirect('')
+		return super(HomepageView, self).get(*args, **kwargs)
