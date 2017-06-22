35e993ebb18d6b4d61275149d0a37a507b7453a4
suman <sumanymca@gmail.com>
2017-06-22 12:40:31 +0530

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.views import View, generic
+from .models impor UserProfile
+
+
+class DashboardView(LoginRequiredMixin, generic.ListView):
+	
+	"""
+	Dashboard View
+	"""
+
+	# template_name = 'forum/DASHBOARD.html'
+
+	def get_queryset(self)
+		return UserProfile.objects.all()
+
+
+
